29f5f0931a5d68849c6bcb015d77ee47e51e4ca6
Patrick Toball <patrick.toball@gmail.com>
2017-01-30 08:16:00 +0100

    result of the mob programming
    was found in the CAS Innovation Space 2017

--- src/main/java/de/cas/mse/address/map/compute/concurrent/MultithreadedCoordinateCalculator.java
+++ src/main/java/de/cas/mse/address/map/compute/concurrent/MultithreadedCoordinateCalculator.java
@@ -1,12 +1,16 @@
 package de.cas.mse.address.map.compute.concurrent;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import de.cas.mse.address.map.compute.CoordinateCalculator;
 import de.cas.mse.address.map.compute.Service;
+import de.cas.mse.address.map.data.Address;
 import de.cas.mse.address.map.data.AddressCatalogue;
 import de.cas.mse.address.map.gui.tasks.ProgressIndicator;
+import javafx.geometry.Point2D;
 
 @Singleton
 public class MultithreadedCoordinateCalculator extends CoordinateCalculator {
@@ -21,8 +25,19 @@ public class MultithreadedCoordinateCalculator extends CoordinateCalculator {
 
 	@Override
 	public void calculateCatalogueCoordinates(ProgressIndicator progressIndicator) {
-		// TODO start coding here
+		int numberAddresses = getAddressCatalogue().getAddresses().size();
+		AtomicInteger counter = new AtomicInteger(0);
+		progressIndicator.updateProgress(0, numberAddresses);
+		getAddressCatalogue().getAddresses().parallelStream()
+				.forEach(address -> setCoordinate(address, counter, progressIndicator, numberAddresses));
 
+	}
+
+	private void setCoordinate(Address address, AtomicInteger count, ProgressIndicator progressIndicator,
+			int numberAddresses) {
+		Point2D point = service.computeCoordinate(address);
+		address.setCoordinate(point);
+		progressIndicator.updateProgress(count.incrementAndGet(), numberAddresses);
 	}
 
 }
--- src/main/java/de/cas/mse/address/map/compute/concurrent/MultithreadedDistanceCalculator.java
+++ src/main/java/de/cas/mse/address/map/compute/concurrent/MultithreadedDistanceCalculator.java
@@ -1,6 +1,11 @@
 package de.cas.mse.address.map.compute.concurrent;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -9,6 +14,7 @@ import de.cas.mse.address.map.compute.DistanceCalculator;
 import de.cas.mse.address.map.compute.Service;
 import de.cas.mse.address.map.data.Address;
 import de.cas.mse.address.map.data.AddressCatalogue;
+import de.cas.mse.address.map.data.Connection;
 import de.cas.mse.address.map.gui.tasks.ProgressIndicator;
 
 @Singleton
@@ -23,9 +29,43 @@ public class MultithreadedDistanceCalculator extends DistanceCalculator {
 	}
 
 	@Override
-	protected void calculateInternal(List<Address> part1, List<Address> part2,
-			ProgressIndicator progressIndicator) {
-		// TODO Start coding here
+	protected void calculateInternal(List<Address> part1, List<Address> part2, ProgressIndicator progressIndicator) {
+		ExecutorService threadPool = Executors.newFixedThreadPool(ConcurrencyParameters.THREAD_POOL_SIZE);
+		AtomicInteger counter = new AtomicInteger(0);
+		IntStream.range(0, part1.size()).forEach(i -> threadPool
+				.execute(new CalculateTask(counter, part1.get(i), part2.get(i), progressIndicator, part1.size())));
+		threadPool.shutdown();
+		try {
+			threadPool.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			// i dont care
+		}
+	}
+
+	private class CalculateTask implements Runnable {
+
+		AtomicInteger counter;
+		Address a1;
+		Address a2;
+		ProgressIndicator progress;
+		int overallAmount;
+
+		public CalculateTask(AtomicInteger counter, Address a1, Address a2, ProgressIndicator progress,
+				int overallAmount) {
+			this.counter = counter;
+			this.a1 = a1;
+			this.a2 = a2;
+			this.progress = progress;
+			this.overallAmount = overallAmount;
+		}
+
+		@Override
+		public void run() {
+			Connection c = service.computeConnection(a1, a2);
+			getAddressCatalogue().getConnections().add(c);
+			progress.updateProgress(counter.incrementAndGet(), overallAmount);
+		}
+
 	}
 
 }
